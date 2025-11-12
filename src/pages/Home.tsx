971c49cbbce0904707f12f78fc8cfc8da4fa68ac
Philbert Loekman <philbert@loekman.com>
2025-11-12 02:20:45 -0800

    Adjust hero image vertical size

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -52,7 +52,7 @@ const Home = () => {
   return (
     <div>
       {/* Hero Section */}
-      <section className="relative min-h-[90vh] flex items-center justify-center overflow-hidden">
+      <section className="relative min-h-screen flex items-center justify-center overflow-hidden">
         <div
           className="absolute inset-0 bg-cover bg-center"
           style={{
